fdae958daf1e4527db872ec5d83b47f7f6631877
Alejandro Upegui <alejandro.upegui@ias.com.co>
2020-06-25 22:13:35 -0500

    Configuration of Mongodb

--- components/message/store/store.js
+++ components/message/store/store.js
@@ -1,7 +1,17 @@
-const list = [];
+const mongoose = require('mongoose');
+const model = require('../models/models');
+//mongodb+srv://db_user_chat:uN1p7xwP8s2P@clusterchat-8nlmx.mongodb.net/db_chat
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  'mongodb+srv://db_user_chat:uN1p7xwP8s2P@clusterchat-8nlmx.mongodb.net/db_chat',
+  { useNewUrlParser: true }
+);
+console.log(`[db] conectada con exito`);
 
 const addMessage = (message) => {
-  list.push(message);
+  // list.push(message);
+  const myMessage = new model(message);
+  myMessage.save();
 };
 
 const getMessages = () => {
