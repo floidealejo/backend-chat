3b97660f8f6ab4cc102e9c008ef579c68098cab5
Alejandro Upegui Betancur <alejandro.upegui@ias.com.co>
2020-06-26 11:31:30 -0500

    Create and read data from MongoDB

--- components/message/models/models.js
+++ components/message/models/models.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 const mySchema = new Schema({
   user: String,
-  message: {
+  content: {
     type: String,
     required: true,
   },
   date: Date,
 });
 
-const model = mongoose.model('Messages', mySchema);
+const model = mongoose.model("Messages", mySchema);
 module.exports = model;
--- components/message/store/store.js
+++ components/message/store/store.js
@@ -3,19 +3,24 @@ const model = require("../models/models");
 const dotenv = require("dotenv");
 
 dotenv.config({ path: `.env` });
+
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 console.log(`[db] conectada con exito`);
 
 const addMessage = (message) => {
-  // list.push(message);
   const myMessage = new model(message);
   myMessage.save();
 };
 
-const getMessages = () => {
-  return list;
+const getMessages = async () => {
+  // return list;
+  const messages = await model.find();
+  return messages;
 };
 module.exports = {
   add: addMessage,
